388e6124cbf563f1b301a04a64e05b324a5eded2
Rafael Nobre <rafaelmedeirosnobre2001@gmail.com>
2023-07-09 17:16:19 -0300

    fix: deploy

--- alinhamento_sequencias/src/index.tsx
+++ alinhamento_sequencias/src/index.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <HashRouter basename="/">
-      <App />
-    </HashRouter>
+    <App />
 );
